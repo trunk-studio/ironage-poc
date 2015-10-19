d3fcaf2664a2375cd8f7cc2d8e9e09e18c3c7517
smlsunxie <smlsun@gmail.com>
2015-10-20 01:27:54 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,8 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('webserver', function() {
-<<<<<<< HEAD
-  gulp.src('.')
-=======
+
   gulp.src('./app/')
->>>>>>> c7ec0e5218a26a439a284836e2ceb49816e662bc
     .pipe(webserver({
       livereload: true,
       directoryListing: {
